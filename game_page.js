45099e295ad6bb509e01c2815e79b25270465d20
Shwetha <104769790+Shwetha2008@users.noreply.github.com>
2022-09-26 20:16:00 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -7,11 +7,12 @@ document.getElementById("player_2_name").innerHTML = player_2_name + ":";
 player_1_score = 0;
 player_2_score = 0;
 
-document.getElementById("player_1_score").innerHTML = player1_score;
-document.getElementById("player_2_score").innerHTML = player2_score;
+document.getElementById("player_1_score").innerHTML = player_1_score;
 
-document.getElementById("player_question").innerHTML = "Question Turn : " + "player_1_name";
-document.getElementById("player_answer").innerHTML = "Answer Turn : " + "player_2_name";
+document.getElementById("player_2_score").innerHTML = player_2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn : " + player_1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn : " + player_2_name;
 
 
 function send()
@@ -29,4 +30,52 @@ function send()
 
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+
+
+question_turn = "player1";
+
+answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check").value;
+
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+        player_1_score = player_1_score + 1;
+        document.getElementById("player_1_score").innerHTML = player_1_score;
+    }
+    else
+    {
+        player_2_score = player_2_score + 1;
+        document.getElementById("player_2_score").innerHTML = player_2_score;
+    }
+}
+
+    if(question_turn == "player1")
+    {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn : " + player_2_name;
+    }
+    else
+    {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn : " + player_1_name;
+    }
+
+    if(answer_turn == "player1")
+    {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player_2_name;
+    }
+    else
+    {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player_1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
 }
